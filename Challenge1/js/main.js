749782944efd44dad3ebf8d798161cb59f278774
Joaquin Centeno <centenojoaco01@gmail.com>
2022-12-29 12:16:50 -0300

    Comentado del código main.js

--- Challenge1/js/main.js
+++ Challenge1/js/main.js
@@ -1,3 +1,4 @@
+//Declaracion de variables
 var input = document.querySelector('.input');
 var btnEncriptar = document.querySelector('.button_one');
 var btnDesencriptar = document.querySelector('.button_two');
@@ -11,15 +12,18 @@ var mensajeDos = document.querySelector('.message_two');
 
 //Encriptación
 btnEncriptar.addEventListener('click',()=>{
-    let textoIngresado = input.value;
-    let encriptado = "";
+    let textoIngresado = input.value; //Se toma el valor del input
+    let encriptado = ""; //Se crea variable de encriptado
 
-    if(textoIngresado.length == 0){
+    if(textoIngresado.length == 0){ //Se verifica que se haya ingresado un valor en el input
+        //En caso de NO haber ingresado valores
         imagen.setAttribute("src","src/muñeco.svg");
         mensajeDos.style.display = "block";
         mensajeUno.textContent = "Ningun mensaje fue encontrado";
+        mensajeDos.textContent = "Ingresa el texto que desees encriptar o desencriptar";
         btnCopiar.style.display = "none";
     }else{
+        //En caso de haber ingresado valores
         for (const letra of textoIngresado) {
             switch (letra) {
                 case "a":
@@ -42,7 +46,7 @@ btnEncriptar.addEventListener('click',()=>{
                     break;
             }
         }
-    
+        //Se muestra el mensaje encriptado
         imagen.setAttribute("src","");
         mensajeDos.style.display = "none";
         mensajeUno.textContent = encriptado;
@@ -55,16 +59,20 @@ btnEncriptar.addEventListener('click',()=>{
 
 //Desencriptacion
 btnDesencriptar.addEventListener('click',()=>{
-    let textoIngresado = input.value;
+    let textoIngresado = input.value; //Se toma el valor del input
 
-    if(textoIngresado.length == 0){
+    if(textoIngresado.length == 0){ //Se verifica que se haya ingresado valores en el input
+        //En caso de no haber ingresado valores
         imagen.setAttribute("src","src/muñeco.svg");
         mensajeDos.style.display = "block";
         mensajeUno.textContent = "Ningun mensaje fue encontrado";
+        mensajeDos.textContent = "Ingresa el texto que desees encriptar o desencriptar";
         btnCopiar.style.display = "none";
     }else{
-        let desencriptado = textoIngresado.replaceAll("ai","a").replaceAll("enter","e").replaceAll("imes","i").replaceAll("ober","o").replaceAll("ufat","u");
+        //En caso de haber ingresado valores
+        let desencriptado = textoIngresado.replaceAll("ai","a").replaceAll("enter","e").replaceAll("imes","i").replaceAll("ober","o").replaceAll("ufat","u"); //Se reemplazan los valores correspondientes
 
+        //Se muestra el mensaje desencriptado
         imagen.setAttribute("src","");
         mensajeDos.style.display = "none";
         mensajeUno.textContent = desencriptado;
@@ -77,7 +85,7 @@ btnDesencriptar.addEventListener('click',()=>{
 
 //Copiado de texto
 btnCopiar.addEventListener('click',()=>{
-    navigator.clipboard.writeText(mensajeUno.textContent);
+    navigator.clipboard.writeText(mensajeUno.textContent); //Se copia el texto que se encuentra en el mensajeUno
 
     btnCopiar.style.display ="none";
     mensajeDos.style.display = "block";
@@ -89,14 +97,14 @@ btnCopiar.addEventListener('click',()=>{
 
 //Controla el uso de mayusculas
 window.addEventListener("keyup",(event)=>{
-    let bloq = event.getModifierState("CapsLock");
+    let bloq = event.getModifierState("CapsLock"); //Se toma el valor si se ha presionado la tecla Bloq Mayus (true o false)
 
-    if(bloq){
+    if(bloq){ //En caso de que la tecla haya sido presionada se deshabilitan los botones e input
         alerta.style.color = "red";
         input.setAttribute("disabled","disabled");
         btnEncriptar.setAttribute("disabled","disabled");
         btnDesencriptar.setAttribute("disabled","disabled");
-    }else{
+    }else{ //En caso de que la tecla no este presionada se habilitarán los botones e input
         alerta.style.color = "black";
         input.removeAttribute("disabled");
         btnEncriptar.removeAttribute("disabled");
